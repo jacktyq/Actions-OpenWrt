f4b9f9a9ad4fc3f2a7e07062e15560ce2bc14e62
jacktyq <64889985+jacktyq@users.noreply.github.com>
2020-05-07 00:43:07 +0800

    change

--- diy.sh
+++ diy.sh
@@ -9,7 +9,4 @@
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/jerrykuku/lua-maxminddb package/helloworld/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr package/helloworld/luci-app-vssr
-cd package/lean
-rm -rf luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
 sed -i 's/192.168.1.1/192.168.1.101/g' package/base-files/files/bin/config_generate
